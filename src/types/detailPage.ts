c2f24c9f90daf368f3362ade36853c4bfd3bf2b1
hojpegmafia <theghtjd@gmail.com>
2026-04-02 23:16:30 +0900

    미션3 상세 페이지 완료

---
+++ src/types/detailPage.ts
@@ -0,0 +1,16 @@
+export type detailPage = {
+   id:number;
+   original_title:string;
+   overview:string;
+   poster_path:string;
+   release_date:string;
+   vote_average:number;
+   runtime:number;
+   backdrop_path:string;
+};
+
+export type detailPage2 = {
+    name:string;
+    profile_path:string;
+    character:string;
+}
